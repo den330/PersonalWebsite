0275f923b180e5c22db187f864605f7a67fbd135
Yaxin Yuan <jack.yuan@emmetros.com>
2019-06-16 19:00:31 -0400

    color update

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$('.nvskill').hover(function(){
+	$('#skillSec').css('background-color', 'yellow');
+}, function(){
+	$('#skillSec').css('background-color', 'white');
+})
+
+$('.nvwork').hover(function(){
+	$('#workSec').css('background-color', 'yellow');
+}, function(){
+	$('#workSec').css('background-color', 'white');
+})
+
