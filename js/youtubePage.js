c0bfce7ee8ff2cc1752dcd87cfda369ec61d476d
Yaxin Yuan <jack.yuan@emmetros.com>
2019-07-19 21:37:49 -0400

    vue

---
+++ js/youtubePage.js
@@ -0,0 +1,10 @@
+var youtubeLinks = ["https://www.youtube.com/embed/tgbNymZ7vqY",
+"https://www.youtube.com/embed/tgbNymZ7vqY",
+"https://www.youtube.com/embed/tgbNymZ7vqY"];
+
+new Vue({
+	el: '#videos',
+	data:{
+		links: youtubeLinks
+	}
+})
